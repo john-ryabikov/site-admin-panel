407678beaddfa02f95b1658961520f591184b227
john-ryabikov <zhenya.ryabikov@mail.ru>
2025-09-13 13:41:02 +0300

    v.0.1.0 (add router)

--- src/app/admin-panel/_components/SectionEdit/SectionEdit.tsx
+++ src/app/admin-panel/_components/SectionEdit/SectionEdit.tsx
@@ -3,6 +3,7 @@
 import { Section } from "@prisma/client"
 import { useState } from "react"
 import { publicChanges } from "../../utils/public-changes" 
+import { useRouter } from "next/navigation"
 
 interface Props {
     section: Section
@@ -13,12 +14,14 @@ export default function SectionEdit({ section } : Props) {
     const [edit, setEdit] = useState(false)
     const [valueInput, setValueInput] = useState(section.title)
 
+    const router = useRouter()
+
     const bindPublicChange = publicChanges.bind(null, section.id)
 
     const publicTitle = (data: FormData) => {
       bindPublicChange(data)
       setEdit(prev => !prev)
-      window.location.reload()
+      router.refresh()
     }
 
     return (
